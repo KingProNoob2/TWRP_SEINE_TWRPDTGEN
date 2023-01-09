273f90eed2fd4dae4dc6e8880092d9bdabf93e7e
KingProNoob2 <71173653+KingProNoob2@users.noreply.github.com>
2023-01-09 13:28:39 +0100

    Add some HALs

--- device.mk
+++ device.mk
@@ -16,7 +16,12 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.0-impl-wrapper.recovery \
+    android.hardware.boot@1.0-impl-wrapper \
+    android.hardware.boot@1.0-impl-recovery \
+    bootctrl.trinket \
+    bootctrl.trinket.recovery \
 
 PRODUCT_PACKAGES += \
     bootctrl.trinket
