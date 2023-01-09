2d9879547fe9dc530c83684fc86332f4ac06b377
KingProNoob2 <71173653+KingProNoob2@users.noreply.github.com>
2023-01-09 11:55:51 +0100

    Add files via upload

---
+++ omni_pdx201.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from pdx201 device
+$(call inherit-product, device/sony/pdx201/device.mk)
+
+PRODUCT_DEVICE := pdx201
+PRODUCT_NAME := omni_pdx201
+PRODUCT_BRAND := sony
+PRODUCT_MODEL := Xperia 10 II
+PRODUCT_MANUFACTURER := sony
+
+PRODUCT_GMS_CLIENTID_BASE := android-google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="redfin-user 12 SPB5.210812.002 7671067 release-keys"
+
+BUILD_FINGERPRINT := google/redfin/redfin:12/SPB5.210812.002/7671067:user/release-keys
